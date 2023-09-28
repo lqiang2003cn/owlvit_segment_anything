22386c0577ea629369d2a67af978a9ea414da622
lqiang2003cn <randycoq@163.com>
2023-09-28 11:25:17 +0800

    c

---
+++ lq_test/det.py
@@ -0,0 +1,5 @@
+from transformers import OwlViTProcessor, OwlViTForObjectDetection
+
+model = OwlViTForObjectDetection.from_pretrained("google/owlvit-base-patch32")
+processor = OwlViTProcessor.from_pretrained("google/owlvit-base-patch32")
+
